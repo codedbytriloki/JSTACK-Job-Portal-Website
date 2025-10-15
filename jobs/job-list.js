0f397b097b70069400c1654aefcea91a5bb55d42
codedbytriloki <trilokisaini61@gmail.com>
2025-10-15 19:11:38 +0530

    Add Job Search file

---
+++ jobs/job-list.js
@@ -0,0 +1,10 @@
+const jCategory = [
+  {
+    index: 0,
+    image: "../images/google.png",
+    title: "Web Development",
+    rate: '$900-1200/m',
+    av: "Full Time",
+  },
+  
+]
---
+++ jobs/jobSearch.js
@@ -0,0 +1,10 @@
+const categories = Array.from(jCategory)
+
+const displayItems = () => {
+  const rootElement = document.getElementById("root");
+  rootElement.innerHTML = "";
+
+  items.forEach((item) => {
+    const {} = item
+  })
+}
