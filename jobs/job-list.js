826a4ce3fc4b2d83c5cebb3f64371171a79f6c49
codedbytriloki <trilokisaini61@gmail.com>
2025-10-17 18:52:56 +0530

    Add some Changes

--- jobs/job-list.js
+++ jobs/job-list.js
@@ -5,6 +5,29 @@ const jCategory = [
     title: "Web Development",
     rate: '$900-1200/m',
     av: "Full Time",
+    company: "Google",
+    location: "USA",
+    vacancy: "01",
+    hours: "50h / Week",
+    description: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Delectus, aperiam iure culpa deserunt sunt, magnam pariatur sed porro sit doloribus animi saepe laudantium! Cumque reprehenderit quidem, repellendus praesentium inventore quibusdam?Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dicta magnam vitae illo corrupti rem ea ipsa ipsam dolores, libero provident, eaque, aspernatur in ab molestias esse atque voluptatibus soluta recusandae! Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptas optio officiis est minima, harum nostrum nihil excepturi recusandae, molestiae error dolorum atque. Blanditiis maiores ipsum saepe! Magnam debitis rerum reprehenderit?",
+    workplace: "Work from home",
+    education: "Bachelor degree in any Computer Science Course.",
+    experience: "2 to 5 year(5)"
   },
-  
+  {
+    index: 1,
+    image: "../images/uber.png",
+    title: "Business Associate",
+    rate: '$900-1200/m',
+    av: "Part Time",
+    company: "Uber",
+    location: "USA",
+    vacancy: "04",
+    hours: "40h / week",
+    description: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Delectus, aperiam iure culpa deserunt sunt, magnam pariatur sed porro sit doloribus animi saepe laudantium! Cumque reprehenderit quidem, repellendus praesentium inventore quibusdam?Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dicta magnam vitae illo corrupti rem ea ipsa ipsam dolores, libero provident, eaque, aspernatur in ab molestias esse atque voluptatibus soluta recusandae! Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptas optio officiis est minima, harum nostrum nihil excepturi recusandae, molestiae error dolorum atque. Blanditiis maiores ipsum saepe! Magnam debitis rerum reprehenderit?",
+    workplace: "Work from office",
+    education: "Degree in any Computer Science Course",
+    experience: "1 to 5 years(s)"
+  },
+
 ]
--- jobs/jobSearch.js
+++ jobs/jobSearch.js
@@ -1,10 +1,32 @@
-const categories = Array.from(jCategory)
+const categories = Array.from(jCategory);
 
-const displayItems = () => {
+document.getElementById('searchBar').addEventListener('keyup', (e) => {
+  const searchData = e.target.value.toLowerCase();
+  const filterData = categories.filter((item) =>
+    item.title.toLocaleLowerCase().includes(searchData)
+  );
+  displayItems(filterData);
+});
+
+const displayItems = (items) => {
   const rootElement = document.getElementById("root");
   rootElement.innerHTML = "";
-
+  // console.log(categories);
   items.forEach((item) => {
-    const {} = item
-  })
+    const { index, image, title, rate, av } = item
+    const jList = document.createElement('div');
+    jList.className = "jList";
+    jList.innerHTML = `
+      <img src="${image}" alt="">
+      <h3>${title}</h3>
+      <p>${rate}/m</p>
+      <span id="key">${av}</span>
+      `
+    rootElement.appendChild(jList);
+    jList.addEventListener('click', () => {
+      window.location.href = `job-details.html?id=${index}`;
+    })
+  });
 }
+
+displayItems(categories);
